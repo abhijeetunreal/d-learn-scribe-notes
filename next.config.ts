63833552ff231a98b8728b04bd447cfcc5e7649f
ABHIJEET <71241536+abhijeetunreal@users.noreply.github.com>
2025-04-04 09:16:23 +0530

    dj

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Enable static export mode:
+    output: 'export', 
+    distDir: 'out',
+  };
+  
+  module.exports = nextConfig;
